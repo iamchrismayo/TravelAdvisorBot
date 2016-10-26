545b92c5394d017b5e4c7d40d10bcc7ab5d636fb
Chris Mayo <c_d_mayo@hotmail.com>
2016-10-26 13:40:10 -0700

    Rewired the Post/Handlemessage to focus on wiring up LUIS dialog flow.

--- TravelAdvisorBot/Controllers/MessagesController.cs
+++ TravelAdvisorBot/Controllers/MessagesController.cs
@@ -20,9 +20,7 @@ namespace TravelAdvisorBot
         /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
-            if (activity.Type == ActivityTypes.Message || 
-                activity.Type == ActivityTypes.ContactRelationUpdate || 
-                activity.Type == ActivityTypes.ConversationUpdate)
+            if (activity.Type == ActivityTypes.Message)
             {
                 await Conversation.SendAsync(activity, () => new RootDialog());
             }
@@ -30,6 +28,7 @@ namespace TravelAdvisorBot
             {
                 this.HandleSystemMessage(activity);
             }
+
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
         }
@@ -41,7 +40,17 @@ namespace TravelAdvisorBot
                 // Implement user deletion here
                 // If we handle user deletion, return a real message
             }
-
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
             else if (message.Type == ActivityTypes.Typing)
             {
                 // Handle knowing tha the user is typing
