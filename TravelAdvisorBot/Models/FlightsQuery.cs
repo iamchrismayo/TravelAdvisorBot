b1a3b73272ff3d3ae8f23934cb594fa8e007f82f
Chris Mayo <c_d_mayo@hotmail.com>
2016-10-27 09:47:02 -0700

    Added simple flight query class.

---
+++ TravelAdvisorBot/Models/FlightsQuery.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Microsoft.Bot.Builder.FormFlow;
+
+namespace TravelAdvisorBot.Models
+{
+    [Serializable]
+    public class FlightsQuery
+    {
+        [Prompt("Please enter your {&}")]
+        public string DepartureCity { get; set; }
+
+        [Prompt("Please enter your {&}")]
+        public string ArrivalCity { get; set; }
+
+        [Prompt("Please enter your {&}")]
+        public DateTime DepartureDate { get; set; }
+
+        [Prompt("Please enter your {&}")]
+        public DateTime ReturnDate { get; set; }
+    }
+}
