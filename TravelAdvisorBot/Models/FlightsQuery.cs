c94742305d3d5c66550590e36c921abe0f5d768b
Chris Mayo <c_d_mayo@hotmail.com>
2016-11-02 12:57:02 -0700

    Removed prompts for testing.

--- TravelAdvisorBot/Models/FlightsQuery.cs
+++ TravelAdvisorBot/Models/FlightsQuery.cs
@@ -8,31 +8,18 @@ using Microsoft.Bot.Builder.FormFlow;
 namespace TravelAdvisorBot.Models
 {
     [Serializable]
-    // TemplateUsage.
-        //DateTime
-        //Not understood.
     public class FlightsQuery
     {
-        [Prompt("Where are you flying from?")]
+        //[Prompt("Where are you flying from?")]
         public string DepartureCity { get; set; }
 
-        [Prompt("Where are you flying to?")]
+        //[Prompt("Where are you flying to?")]
         public string ReturnCity { get; set; }
 
-        // Can supply multiple to chose one at randome.
-        [Prompt("What date would you like to leave?")]
-        // Used to replace field in Pattern Language.
-        //[Describe("departure date")]
-        //[Pattern("regex goes here...")]
-        //Numeric
-        //Optional
-        //Pattern
-        //Prompt
-        //Template
-        //Terms
+        //[Prompt("What date would you like to leave?")]
         public string DepartureDate { get; set; }
 
-        [Prompt("What date would you like to return?")]
+        //[Prompt("What date would you like to return?")]
         public string ReturnDate { get; set; }
     }
 }
