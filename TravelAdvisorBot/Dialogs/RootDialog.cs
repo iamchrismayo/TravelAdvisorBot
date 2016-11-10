0fe318546e87ee77823516265fe29a52d1bdf6b7
Chris Mayo <c_d_mayo@hotmail.com>
2016-11-10 15:21:31 -0800

    Includes feature to show flight results.

--- TravelAdvisorBot/Dialogs/RootDialog.cs
+++ TravelAdvisorBot/Dialogs/RootDialog.cs
@@ -1,10 +1,18 @@
-﻿namespace TravelAdvisorBot.Dialogs
+﻿using System;
+using System.Globalization;
+using TravelAdvisorBot.Models;
+
+namespace TravelAdvisorBot.Dialogs
 {
     using Microsoft.Bot.Builder.Dialogs;
     using Microsoft.Bot.Connector;
     using System;
     using System.Threading.Tasks;
     using Models;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Web;
 
 #pragma warning disable 1998
 
@@ -76,14 +84,84 @@
             {
                 this.flightsQuery.ReturnDate = await result;
 
-                await context.PostAsync($"Flight Query: {this.flightsQuery.DepartureCity}");
+                var flights = await this.GetFlightsAsync(flightsQuery);
 
-                context.Wait(this.MessageReceivedAsync);
+                await context.PostAsync($"OK, I found {flights.Count()} flights.");
+
+                var resultMessage = context.MakeMessage();
+                resultMessage.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+                resultMessage.Attachments = new List<Attachment>();
+
+                foreach (var flight in flights)
+                {
+                    HeroCard heroCard = new HeroCard()
+                    {
+                        Title = $"{this.ToTitleCase(flight.DepartureAirport)} to {this.ToTitleCase(flight.ReturnAirport)}",
+                        Subtitle = $"${flight.Price}",
+                        Images = new List<CardImage>()
+                        {
+                            new CardImage() { Url = flight.Image }
+                        },
+                        Buttons = new List<CardAction>()
+                        {
+                            new CardAction()
+                            {
+                                Title = "More details",
+                                Type = ActionTypes.OpenUrl,
+                                Value = $"https://www.bing.com/search?q=flights+from+" + HttpUtility.UrlEncode(flight.DepartureAirport) + "+to+" + HttpUtility.UrlEncode(flight.DepartureAirport)
+                            }
+                        }
+                    };
+
+                    resultMessage.Attachments.Add(heroCard.ToAttachment());
+                }
+
+                await context.PostAsync(resultMessage);
+
             }
             catch (TooManyAttemptsException)
             {
                 context.Wait(this.MessageReceivedAsync);
             }
+            finally
+            {
+                context.Wait(this.MessageReceivedAsync);
+            }
+        }
+
+        private string ToTitleCase(string str)
+        {
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(str.ToLower());
+        }
+
+        private async Task<IEnumerable<Flight>> GetFlightsAsync(FlightsQuery searchQuery)
+        {
+            var flights = new List<Flight>();
+
+            // Filling in the flight details manually for demo purposes...
+            for (int i = 0; i < 5; i++)
+            {
+                var random = new Random(i);
+                Flight flight = new Flight()
+                {
+                    DepartureAirport = $"{searchQuery.DepartureCity}",
+                    DepartureAirline = "Alaska",
+                    DepartureDateTime = DateTime.Parse(searchQuery.DepartureDate),
+                    DepartureFlightNumber = random.Next(101, 9999).ToString(),
+                    ReturnAirport = $"{searchQuery.ReturnCity}",
+                    ReturnAirline = "United",
+                    ReturnDateTime = DateTime.Parse(searchQuery.ReturnDate),
+                    ReturnFlightNumber = random.Next(101, 9999).ToString(),
+                    Price = random.Next(80, 450),
+                    Image = $"https://placeholdit.imgix.net/~text?txtsize=35&txt=Flight+{i + 1}&w=200&h=100"
+                };
+
+                flights.Add(flight);
+            }
+
+            flights.Sort((f1, f2) => f1.Price.CompareTo(f2.Price));
+
+            return flights;
         }
     }
 }
